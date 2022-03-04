f257d648c079be361171b22d7ce2278519944373
relight31 <halfbcsina@yahoo.com.sg>
2022-03-04 14:59:32 +0800

    RedisConfig and added dependencies to pom.xml

---
+++ src/main/java/sg/edu/nus/iss/LibCatalog/config/RedisConfig.java
@@ -0,0 +1,52 @@
+package sg.edu.nus.iss.LibCatalog.config;
+
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    private Logger logger = Logger.getLogger(RedisConfig.class.getName());
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private Optional<Integer> redisPort;
+
+    @Bean
+    @Scope("singleton")
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+
+        config.setHostName(redisHost);
+        config.setPort(redisPort.get());
+        logger.log(Level.INFO, "Successfully set Host " + redisHost + " and Port: " + redisPort);
+
+        final JedisClientConfiguration jedisClient = JedisClientConfiguration.builder().build();
+        final JedisConnectionFactory jedisFac = new JedisConnectionFactory(config, jedisClient);
+        jedisFac.afterPropertiesSet();
+
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisFac);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        RedisSerializer<Object> serializer = new JdkSerializationRedisSerializer(getClass().getClassLoader());
+
+        template.setValueSerializer(serializer);
+
+        return template;
+    }
+}
