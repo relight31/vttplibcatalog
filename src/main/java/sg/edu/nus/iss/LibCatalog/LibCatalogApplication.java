dfcdac3635e0126ba308ab8c7ed6f138bb8a6316
relight31 <halfbcsina@yahoo.com.sg>
2022-03-04 14:02:20 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/LibCatalog/LibCatalogApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.LibCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/LibCatalog/LibCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.LibCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
