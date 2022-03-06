9735ef8cfcfc1d5167444ec900873dfa33bf7d78
Russell <halfbcsina@yahoo.com.sg>
2022-03-06 15:59:13 +0800

    landing page with GetMapping

--- src/main/java/sg/edu/nus/iss/LibCatalog/controller/LibCatalogController.java
+++ src/main/java/sg/edu/nus/iss/LibCatalog/controller/LibCatalogController.java
@@ -1,9 +1,15 @@
 package sg.edu.nus.iss.LibCatalog.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LibCatalogController {
+    private Logger logger = Logger.getLogger(LibCatalogController.class.getName());
     /*
      * TODO
      * functionalies:
@@ -13,4 +19,11 @@ public class LibCatalogController {
      * pagination
      * change num of books in a search
      */
+
+    @GetMapping("/")
+    public String indexPage(Model model) {
+        logger.log(Level.INFO, "Show landing page");
+        model.addAttribute("helloWorld", "Hello World");
+        return "index";
+    }
 }
