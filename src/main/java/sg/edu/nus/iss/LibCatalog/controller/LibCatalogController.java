53a918df7c47fcbaf9f3d097bea4dc2f2099035b
Russell <halfbcsina@yahoo.com.sg>
2022-03-07 15:19:06 +0800

    added search function to controller

--- src/main/java/sg/edu/nus/iss/LibCatalog/controller/LibCatalogController.java
+++ src/main/java/sg/edu/nus/iss/LibCatalog/controller/LibCatalogController.java
@@ -1,15 +1,26 @@
 package sg.edu.nus.iss.LibCatalog.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import sg.edu.nus.iss.LibCatalog.model.Book;
+import sg.edu.nus.iss.LibCatalog.service.LibService;
 
 @Controller
 public class LibCatalogController {
     private Logger logger = Logger.getLogger(LibCatalogController.class.getName());
+
+    @Autowired
+    LibService service;
+
     /*
      * TODO
      * functionalies:
@@ -28,4 +39,21 @@ public class LibCatalogController {
     }
 
     // http://localhost:8080/searchTitle?searchTerm=&searchType=author&sortType=forward
+
+    @GetMapping("/search")
+    public String searchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType,
+            @RequestParam String sortType) {
+        List<Book> result = new ArrayList<Book>();
+
+        if (searchType.equals("author")) {
+            result = service
+                    .sortByAuthor(searchTerm, sortType.equals("forward"));
+        } else {
+            result = service
+                    .sortByTitle(searchTerm, sortType.equals("forward"));
+        }
+
+        model.addAttribute("books", result);
+        return "searchResults";
+    }
 }
